257e1c97e41a5f6d95b351d7229bfeca77dc589a
Farheen_Digitalks <farheendigitalks@gmailo.com>
2025-04-22 15:31:49 +0530

    commit changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     // origin: process.env.CORS_ORIGIN, // your frontend URL
-    origin: 'demo-frontend-lovat.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true
   }));
 
