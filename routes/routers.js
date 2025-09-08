a10637f1ab7323ab7c1ac448f7649b6b519f7b14
farheen-Digitalks <farheendigitalks@gmail.com>
2025-09-08 18:59:08 +0530

    modified

--- routes/routers.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userRouter = require("./user.routes");
-
-router.use("/user", userRouter);
-router.use("/admin", userRouter);
-
-module.exports = router;
