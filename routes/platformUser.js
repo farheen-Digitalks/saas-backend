ca9b87e0ba4a2c02f531559a39965bd83cc22546
farheen-Digitalks <farheendigitalks@gmail.com>
2026-01-30 11:47:07 +0530

    `Update platformUser route to populate role and permissions`

--- routes/platformUser.js
+++ routes/platformUser.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { login, logout, register } from "../controllers/auth.js";
-import { getPlatformUsersHandler, getUserByIdHandler } from "../controllers/platformUser.js";
+import {
+  getPlatformUsersHandler,
+  getUserByIdHandler,
+} from "../controllers/platformUser.js";
 import { authenticate } from "../middlewares/auth.js";
 import { paginationMiddleware } from "../middlewares/pagination.js";
 import PlatformUser from "../models/platformUser.js";
 const router = express.Router();
 
-router.get("/", authenticate, paginationMiddleware(PlatformUser, { populate: "role"}), getPlatformUsersHandler);
+router.get(
+  "/",
+  authenticate,
+  paginationMiddleware(PlatformUser, {
+    populate: { path: "role", populate: { path: "permissions" } },
+  }),
+  getPlatformUsersHandler,
+);
 router.post("/register", register);
 router.get("/:id", getUserByIdHandler);
 router.post("/login", login);
